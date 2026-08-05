d93c5185e8da0300e31f7e1368b045c954d976ae
ChessPieceCat <tiernenbunnell1@gmail.com>
2026-08-05 19:24:46 -0400

    Serve index.html instead of "Hello, World!" on root path

--- cmd/dashboard/main.go
+++ cmd/dashboard/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		file, err := os.ReadFile("/home/tum/Documents/Homelab Dashboard/web/templates/index.html")
+		if err != nil {
+			log.Fatalf("Failed to read index.html: %v", err)
+		}
+		w.Write(file)
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
